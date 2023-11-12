d6d1f6a0664481a31566e33bf2f0e8a6b1cbdae9
AlexaPaneva <paneva.alexa@gmail.com>
2023-11-12 20:38:57 +0200

    Add more of my tasks

---
+++ interfaces/car/Car.java
@@ -0,0 +1,14 @@
+package javaOOP.JavaOOP.interfaces.car;
+
+public interface Car {
+    int TIRES = 4;
+
+    String getModel();
+
+    String getColor();
+
+    Integer getHorsePower();
+
+    String countryProduced();
+}
+
